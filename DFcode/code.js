78d5a321dcf29e1b411cafc45e29dcdd03853009
jhocias2020 <132527674+jhocias2020@users.noreply.github.com>
2023-05-17 21:58:49 -0400

    Add files via upload

---
+++ DFcode/code.js
@@ -0,0 +1,63 @@
+
+function factorialN() {
+    var N = prompt("Ingrese N", 3)
+    var i = 1
+    var resp = 1
+    if (N >= 0) {
+        if (N == 0) {
+            alert("El factorial de N es " + resp)
+        } else {
+            while (N >= i) {
+                resp = resp * i
+                i = i + 1
+            }
+            alert("Factorial de N es: " + resp)
+        }
+    } else {
+        alert("No factorial de un negativo")
+    }
+}
+
+function mayorAyB() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 7))
+    if (A == B) {
+        alert("son iguales")
+    } else {
+        if (A > B) {
+            alert("el mayor es A: " + A)
+        } else {
+            alert("el mayor es B: " + B)
+        }
+    }
+}
+
+function menorAyB() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 7))
+    if (A == B) {
+        alert("son iguales")
+    } else {
+        if (A > B) {
+            alert("el menor es B: " + B)
+        } else {
+            alert("el menor es A: " + A)
+        }
+    }
+}
+
+function obtenerDiv() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 7))
+    var resultado=0
+    resultado=parseInt(A/B)
+    alert(resultado)
+    
+function mostarDigN() {
+    var N = preseInt(prompt("Ingrese N, 9478324))
+    var dig = 0
+    while (N 1= 0 {
+        
+
+
+
