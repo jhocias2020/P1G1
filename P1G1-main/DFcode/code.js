9f571c6e20ced6f68ce476d91d53eaff73858476
jhocias2020 <132527674+jhocias2020@users.noreply.github.com>
2023-05-22 21:37:39 -0400

    Add files via upload

---
+++ P1G1-main/DFcode/code.js
@@ -0,0 +1,107 @@
+
+function factorialN() {
+    var N = prompt("Ingrese N", 3)
+    var i = 1
+    var resp = 1
+    if (N >= 0) {
+        if (N == 0) {
+            alert("El factorial de N es " + resp)
+        } else {
+            while (N >= i) {
+                resp = resp * i
+                i = i + 1
+            }
+            alert("Factorial de N es: " + resp)
+        }
+    } else {
+        alert("No factorial de un negativo")
+    }
+}
+
+function mayorAyB() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 7))
+    if (A == B) {
+        alert("son iguales")
+    } else {
+        if (A > B) {
+            alert("el mayor es A: " + A)
+        } else {
+            alert("el mayor es B: " + B)
+        }
+    }
+}
+
+function menorAyB() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 7))
+    if (A == B) {
+        alert("son iguales")
+    } else {
+        if (A > B) {
+            alert("el menor es B: " + B)
+        } else {
+            alert("el menor es A: " + A)
+        }
+    }
+}
+
+function obtenerDiv() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 7))
+    var resultado=0
+    resultado=parseInt(A/B)
+    alert(resultado)
+/*  
+function mostarDigN() {
+    var N = preseInt(prompt("Ingrese N, 9478324))
+    var dig = 0
+    while (N 1= 0) 
+*/
+        
+function Nmultiplo() {
+    var A = parseInt(prompt("Ingrese A", 10))
+    var B = parseInt(prompt("Ingrese B", 5))
+    
+    if ((N % M) == 0) {
+    } else {
+        alert(N +" no es multiplo de " + M)
+    }
+}
+
+function sumaDigMultiplos3deN(){
+    var N =parseInt(prompt("Ingrese N", 23462314))
+    var sum = 0
+    var dig = 0
+    while (N != 0) {
+        dig = parseInt(N % 10)
+        alert((dig % 3) == 0) {
+        if ((dig % 3) == 0) ¨{}
+
+
+}
+
+function sumaNrosEntreAyB(){
+    var A =parseInt(prompt("ingrese A", 3))
+    var B =parseInt(prompt("Ingrese B", 9))
+    var suma = 0
+
+    while (B <= A) {
+        alert(A)
+        suma = suma + A
+        A = A + 1
+    }
+
+    alert(suma)
+}
+
+function promedioNdeNotas() {
+    var N = parentInt(prompt("Ingrese N", 15))
+    var sumaNota = 0
+    var 1 = 0
+    while(N > 1){
+        1 - 1 + 1
+        var nota = parseInt(piomp("Ingrese Nota"))
+    }
+
+}
