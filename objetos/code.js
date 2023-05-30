0647ea0a1a75272532f5b3d8851b9c353dc0ef7a
jhocias2020 <132527674+jhocias2020@users.noreply.github.com>
2023-05-29 21:59:55 -0400

    Add files via upload

---
+++ objetos/code.js
@@ -0,0 +1,7 @@
+
+function VerPersona(){
+    var persona = new Persona() 
+
+    persona1.setNombre("Cespedes")
+    persona1.setEdad("21")
+    
---
+++ objetos/persona.js
@@ -0,0 +1,66 @@
+//1.- NOMBRE DE LA CLASE
+ class Persona {
+    //2.- PROPIEDAD
+    carnet =0
+    nombre = ""
+    edad = 0
+
+    //3.- COSTRUCTOR, ASIGNADOR VALORES POR DEFECTO
+    Persona(){
+        carnet = 1234567
+        nombre = "MiNombre"
+        edad = 100
+    }
+
+    //4.- SETTERS AND GETTERS - DAR Y OBTENER UN VALOR
+    GetCarnet(){
+        return this.carnet
+    }
+    
+    GetNombre(){
+        return this.nombre
+    }
+
+    GetEdad(){
+        return this.edad
+    }
+
+    SetCarnet(valor){
+        this.carnet = valor
+    }
+
+    SetNombre(valor){
+        this.nombre = valor
+    }
+
+    SetEdad(valor){
+        this.ead = valor
+    }
+
+    //5.- LOS METODOS DE LA CLASE, 0 ....
+    ContarVocales(){
+        var cadena = this.GetNombre()
+        var longitud = cadena.length
+        var i = 0
+        var contar = 0
+        while (longitudCad > i) {
+            var caracter = cadena[i]
+            i++
+            if (caracter == 'a')
+            || caracter == 'e'
+            || caracter == 'i'
+            || caracter == 'o'
+            || caracter == 'u'
+                contar = contar + 1
+            {
+            //alert(caracter)
+        } 
+        //alert("la Frecuencia de 'a, e, i ,o ,u'es" +
+        return contar
+    }
+    
+}    
+    
+
+
+        
